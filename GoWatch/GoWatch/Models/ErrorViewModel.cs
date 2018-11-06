08d3d12dcdc30616b7a8efab8867b79a9a9d1e5d
MR-Design <two7city@gmail.comMR-Design>
2018-11-06 09:48:39 -0600

    Initial Commit

---
+++ GoWatch/GoWatch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoWatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
